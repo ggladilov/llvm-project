f3af6247e9fe3e21edcb7f38e4ca37c50a9abe4b
Nikita Rodionov <nikita.rodionov@honeywell.com>
2022-06-05 02:43:41 +0300

    Fix regarding your comment

--- llvm/lib/Transforms/PeepholeOptimizationCourse/Obfuscate8Bytes/Obfuscate8BytesAdd.cpp
+++ llvm/lib/Transforms/PeepholeOptimizationCourse/Obfuscate8Bytes/Obfuscate8BytesAdd.cpp
@@ -7,37 +7,41 @@
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/PeepholeOptimizationCourse/Obfuscate8BytesNewPassManager.h"
+
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #define DEBUG_TYPE "obfuscate"
 
+
+bool llvm::isAddWithI8(Instruction const* instruction) {
+	bool is8Bit = operand->getType()->isIntegerTy() && (operand->getType()->getIntegerBitWidth() == 8);
+	bool isBinaryAdd = (instruction->getOpcode() == Instruction::Add) && (instruction->getNumOperands() == 2);
+	return is8Bit && isBinaryAdd;
+}
+
+void llvm::doObfuscateI8Add(Instruction* instruction) {
+	auto const& a = instruction->getOperand(0);
+	auto const& b = instruction->getOperand(1);
+	auto const& I8Type = a->getType();
+	
+	IRBuilder<> Builder(instruction);
+	auto mul4 =  Builder.CreateXor(a,b);
+	auto inc2 = Builder.CreateAnd(a, b);
+	auto inc3 = Builder.CreateMul(ConstantInt::get(I8Type, 2), mul4);
+	auto mul3 = Builder.CreateAdd(inc2, inc3);
+	auto mul2 = Builder.CreateMul(ConstantInt::get(I8Type, 39), mul3);
+	auto mul1 = Builder.CreateAdd(mul2, ConstantInt::get(I8Type, 23));
+	auto inc1 = Builder.CreateMul(mul1, ConstantInt::get(I8Type, 151));
+	Instruction *newInst = BinaryOperator::CreateAdd(inc1, ConstantInt::get(I8Type, 111));
+	ReplaceInstWithInst(instruction, newInst);	
+}
+
 using namespace llvm;
 namespace {
 
-	bool isAddWithI8(Instruction const* instruction) {
-		bool is8Bit = operand->getType()->isIntegerTy() && (operand->getType()->getIntegerBitWidth() == 8);
-		bool isBinaryAdd = (instruction->getOpcode() == Instruction::Add) && (instruction->getNumOperands() == 2);
-		return is8Bit && isBinaryAdd;
-	}
-
-	void doObfuscateI8Add(Instruction* instruction) {
-		auto const& a = instruction->getOperand(0);
-		auto const& b = instruction->getOperand(1);
-		auto const& I8Type = a->getType();
-		
-		IRBuilder<> Builder(instruction);
-		auto mul4 =  Builder.CreateXor(a,b);
-		auto inc2 = Builder.CreateAnd(a, b);
-		auto inc3 = Builder.CreateMul(ConstantInt::get(I8Type, 2), mul4);
-		auto mul3 = Builder.CreateAdd(inc2, inc3);
-		auto mul2 = Builder.CreateMul(ConstantInt::get(I8Type, 39), mul3);
-		auto mul1 = Builder.CreateAdd(mul2, ConstantInt::get(I8Type, 23));
-		auto inc1 = Builder.CreateMul(mul1, ConstantInt::get(I8Type, 151));
-		Instruction *newInst = BinaryOperator::CreateAdd(inc1, ConstantInt::get(I8Type, 111));
-		ReplaceInstWithInst(instruction, newInst);	
-	}
 
 
 	struct Obfuscate8Bytes : public FunctionPass {
