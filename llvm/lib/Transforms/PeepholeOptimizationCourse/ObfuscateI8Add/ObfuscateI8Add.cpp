597b7e7d2b0d45ab1777168241ec44a4c786044c
Fedoseevv26 <myiashik@bk.ru>
2022-06-05 18:42:54 +0300

    add reusable code between passes

--- llvm/lib/Transforms/PeepholeOptimizationCourse/ObfuscateI8Add/ObfuscateI8Add.cpp
+++ llvm/lib/Transforms/PeepholeOptimizationCourse/ObfuscateI8Add/ObfuscateI8Add.cpp
@@ -13,6 +13,8 @@
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
+#include "llvm/Transforms/PeepholeOptimizationCourse/ObufscateI8AddFunction.h"
+
 
 using namespace llvm;
 
@@ -43,27 +45,9 @@ namespace{
                 }
 
             
-            auto const& a = binOp->getOperand(0);
-            auto const& b = binOp->getOperand(1);
-            auto Const39 = ConstantInt::get(binOp->getType(), 39);
-            auto Const2 = ConstantInt::get(binOp->getType(), 2);
-            auto Const23 = ConstantInt::get(binOp->getType(), 23);
-            auto Const151 = ConstantInt::get(binOp->getType(), 151);
-            auto Const111 = ConstantInt::get(binOp->getType(), 111);
 
-            IRBuidler<> builder(binOp);
-
-            auto And = builder.CreateAnd(a,b);
-            auto Xor = builder.CreateXor(a,b);
-            auto MulW2 = builder.CreateMul(Xor,Const2);
-            auto Add = builder.CreateAdd(And,MulW2);
-            auto MulW39 = builder.CreateMul(Add,Const39);
-            auto AddW23 = builder.CreateAdd(MulW39,Const23)
-            auto MulW151 = builder.CreateMul(Sum,Const151);
-
-            auto obfuscated = BinaryOperator::CreateAdd(MulW151,Const111);
-            ReplaceInstWithInst(basicBlock->getInstList(), instruction, obfuscated);
-
+            doI8Add(instruction);
+            
             LLVM_DEBUG(dbgs() << *BinaryOperator << " -> " << *obfuscated << '\n');
             changed = true;
     }
--- llvm/lib/Transforms/PeepholeOptimizationCourse/ObfuscateI8AddNewPassManager/ObfuscateI8AddNewPassManager.cpp
+++ llvm/lib/Transforms/PeepholeOptimizationCourse/ObfuscateI8AddNewPassManager/ObfuscateI8AddNewPassManager.cpp
@@ -11,6 +11,7 @@
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 #include "llvm/Transforms/PeepholeOptimizationCourse/ObfuscateI8AddNewPassManager.h"
+#include "llvm/Transforms/PeppholeOptimizationCourse/ObfuscateI8AddFunction.h"
 
 using namespace llvm;
 
@@ -36,26 +37,9 @@ PreservedAnalyses ObfuscateI8AddNewPassManager::run(Function& F, FunctionAnalysi
                     continue;
                 }
 
-            auto const& a = binOp->getOperand(0);
-            auto const& b = binOp->getOperand(1);
-            auto Const39 = ConstantInt::get(binOp->getType(), 39);
-            auto Const2 = ConstantInt::get(binOp->getType(), 2);
-            auto Const23 = ConstantInt::get(binOp->getType(), 23);
-            auto Const151 = ConstantInt::get(binOp->getType(), 151);
-            auto Const111 = ConstantInt::get(binOp->getType(), 111);
 
-            IRBuidler<> builder(binOp);
+            doI8Add(instruction);
 
-            auto And = builder.CreateAnd(a,b);
-            auto Xor = builder.CreateXor(a,b);
-            auto MulW2 = builder.CreateMul(Xor,Const2);
-            auto Add = builder.CreateAdd(And,MulW2);
-            auto MulW39 = builder.CreateMul(Add,Const39);
-            auto AddW23 = builder.CreateAdd(MulW39,Const23)
-            auto MulW151 = builder.CreateMul(Sum,Const151);
-
-            auto obfuscated = BinaryOperator::CreateAdd(MulW151,Const111);
-            ReplaceInstWithInst(basicBlock->getInstList(), instruction, obfuscated);
 
             changed = PreservedAnalyses::none();
             }
