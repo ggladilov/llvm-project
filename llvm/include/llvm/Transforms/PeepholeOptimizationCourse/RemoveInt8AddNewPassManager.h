146f7e4d37649e18d7f0d123cc33023972bfbf4e
enjulia17 <enjulia@mail.ru>
2022-06-06 22:27:36 +0300

    fix CMakeLists, add New Pass Manager

---
+++ llvm/include/llvm/Transforms/PeepholeOptimizationCourse/RemoveInt8AddNewPassManager.h
@@ -0,0 +1,17 @@
+#ifndef LLVM_TRANSFORMS_PEEPHOLE_OBFUSCATE_I8_ADD
+#define LLVM_TRANSFORMS_PEEPHOLE_OBFUSCATE_I8_ADD
+
+#include "llvm/IR/PassManager.h"
+
+namespace llvm {
+
+class ReplaceInt8AddNewPassManager :
+    public PassInfoMixin<ReplaceInt8SumNewPassManager> {
+public:
+    PreservedAnalyses run(Function& function, FunctionAnalysisManager& analysisManager);
+    static bool isRequired() { return true; }
+};
+
+}  // namespace llvm
+
+#endif  // LLVM_TRANSFORMS_PEEPHOLE_OBFUSCATE_I8_ADD
