f170c20d5dd8147a9ce28dd7034b526afddb739a
KutuevR <95222632+KutuevR@users.noreply.github.com>
2022-06-06 23:25:15 +0300

    Changed formula

--- llvm/lib/Transforms/PeepholeOptimizationCourse/Func/Func.cpp
+++ llvm/lib/Transforms/PeepholeOptimizationCourse/Func/Func.cpp
@@ -10,33 +10,22 @@
 namespace llvm {
 
 Instruction* SwapInt8Instruction(BinaryOperator* binOp) {
-                
-                IRBuilder<> B(binOp);
+    IRBuilder<> B(binary_op);
 
-                auto const& lhs = binOp->getOperand(0);
-                auto const& rhs = binOp->getOperand(1);
-                auto const& i8 = binOp->getType();
+    auto const& lhs = binary_op->getOperand(0);
+    auto const& rhs = binary_op->getOperand(1);
+    auto const& i8 = binary_op->getType();
 
-                Instruction *replacement = BinaryOperator::CreateAdd(
-                    B.CreateMul(
-                        B.CreateMul(
-                            ConstantInt::get(i8, 39),
-                            B.CreateAdd(
-                                B.CreateAnd(lhs, rhs),
-                                B.CreateAdd(
-                                    B.CreateMul(
-                                        ConstantInt::get(i8, 2),
-                                        B.CreateXor(lhs, rhs)
-                                    ),
-                                    ConstantInt::get(i8, 23)
-                                )
-                            )
-                        ),
-                        ConstantInt::get(i8, 151)
-                    ),
-                    ConstantInt::get(i8, 111)
-                );
-                return replacement;
+    const auto AndOp1 = B.CreateAnd(lhs, rhs);
+    const auto XorOp1 = B.CreateXor(lhs, rhs);
+    const auto MulOp1 = B.CreateMul(ConstantInt::get(i8, 2), XorOp1);
+    const auto AddOp1 = B.CreateAdd(AndOp1, MulOp1);
+    const auto MulOp2 = B.CreateMul(ConstantInt::get(i8, 39), AddOp1);
+    const auto AddOp2 = B.CreateAdd(MulOp2, ConstantInt::get(i8, 23));
+    const auto MulOp3 = B.CreateMul(AddOp2, ConstantInt::get(i8, 151));
+    Instruction *replacement = BinaryOperator::CreateAdd(MulOp3, ConstantInt::get(i8, 111));
+    
+    return replacement;
 }
 
 }
