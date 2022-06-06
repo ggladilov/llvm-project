57625ea9814e9f2e40fe126b7eed61827bcf50fa
Apple228 <61735167+Apple228@users.noreply.github.com>
2022-06-06 23:19:12 +0300

    fixed order

--- llvm/lib/Transforms/PeepholeOptimizationCourse/ObfuscI8Func/ObfuscI8Func.cpp
+++ llvm/lib/Transforms/PeepholeOptimizationCourse/ObfuscI8Func/ObfuscI8Func.cpp
@@ -34,25 +34,15 @@ Instruction* obfuscInstruction(BinaryOperator* binary_op) {
     auto const& rhs = binary_op->getOperand(1);
     auto const& i8 = binary_op->getType();
 
-    return BinaryOperator::CreateAdd(
-        B.CreateMul(
-            B.CreateMul(
-                ConstantInt::get(i8, 39),
-                B.CreateAdd(
-                    B.CreateAnd(lhs, rhs),
-                    B.CreateAdd(
-                        B.CreateMul(
-                            ConstantInt::get(i8, 2),
-                            B.CreateXor(lhs, rhs)
-                        ),
-                        ConstantInt::get(i8, 23)
-                    )
-                )
-            ),
-            ConstantInt::get(i8, 151)
-        ),
-        ConstantInt::get(i8, 111)
-    );;
+    const auto AndOp1 = B.CreateAnd(lhs, rhs);
+    const auto XorOp1 = B.CreateXor(lhs, rhs);
+    const auto MulOp1 = B.CreateMul(ConstantInt::get(i8, 2), XorOp1);
+    const auto AddOp1 = B.CreateAdd(AndOp1, MulOp1);
+    const auto MulOp2 = B.CreateMul(ConstantInt::get(i8, 39), AddOp1);
+    const auto AddOp2 = B.CreateAdd(MulOp2, ConstantInt::get(i8, 23));
+    const auto MulOp3 = B.CreateMul(AddOp2, ConstantInt::get(i8, 151));
+
+    return BinaryOperator::CreateAdd(MulOp3, ConstantInt::get(i8, 111));
 }
 
 }
