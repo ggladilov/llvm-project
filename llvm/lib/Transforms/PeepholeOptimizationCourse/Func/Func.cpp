d9834225d977f299a87725a48c21ddf6d4008746
brimst0ne <shadowmoon152@gmail.com>
2022-06-06 23:21:51 +0300

    formula fix

--- llvm/lib/Transforms/PeepholeOptimizationCourse/Func/Func.cpp
+++ llvm/lib/Transforms/PeepholeOptimizationCourse/Func/Func.cpp
@@ -15,25 +15,15 @@ Instruction* ReplaceInt8Instruction(BinaryOperator* binOp) {
                 auto const& rhs = binOp->getOperand(1);
                 auto const& i8 = binOp->getType();
 
-                Instruction *replacement = BinaryOperator::CreateAdd(
-                    B.CreateMul(
-                        B.CreateMul(
-                            ConstantInt::get(i8, 39),
-                            B.CreateAdd(
-                                B.CreateAnd(lhs, rhs),
-                                B.CreateAdd(
-                                    B.CreateMul(
-                                        ConstantInt::get(i8, 2),
-                                        B.CreateXor(lhs, rhs)
-                                    ),
-                                    ConstantInt::get(i8, 23)
-                                )
-                            )
-                        ),
-                        ConstantInt::get(i8, 151)
-                    ),
-                    ConstantInt::get(i8, 111)
-                );
+                auto mul4 = Builder.CreateXor(lhs, rhs);
+                auto inc2 = Builder.CreateAnd(lhs, rhs);
+	            auto inc3 = Builder.CreateMul(ConstantInt::get(i8, 2), mul4);
+	            auto mul3 = Builder.CreateAdd(inc2, inc3);
+	            auto mul2 = Builder.CreateMul(ConstantInt::get(i8, 39), mul3);
+	            auto mul1 = Builder.CreateAdd(mul2, ConstantInt::get(i8, 23));
+	            auto inc1 = Builder.CreateMul(mul1, ConstantInt::get(i8, 151));
+
+                Instruction *replacement = BinaryOperator::CreateAdd(inc1, ConstantInt::get(i8, 111));
 
                 return replacement;
 }
