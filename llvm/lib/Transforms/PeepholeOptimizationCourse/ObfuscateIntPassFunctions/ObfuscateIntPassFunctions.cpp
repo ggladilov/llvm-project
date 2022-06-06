33fa9c08c284e3407f128cdf9b34c95a99d8594b
Sindeguru <54904300+Sindeguru@users.noreply.github.com>
2022-06-06 22:46:46 +0300

    fix

--- llvm/lib/Transforms/PeepholeOptimizationCourse/ObfuscateIntPassFunctions/ObfuscateIntPassFunctions.cpp
+++ llvm/lib/Transforms/PeepholeOptimizationCourse/ObfuscateIntPassFunctions/ObfuscateIntPassFunctions.cpp
@@ -10,32 +10,23 @@
 namespace llvm {
 
 Instruction* PassInstruction(BinaryOperator* binary_operator) {
-                IRBuilder<> B(binary_operator);
-                auto const& lhs = binary_operator->getOperand(0);
-                auto const& rhs = binary_operator->getOperand(1);
-                auto const& i8 = binary_operator->getType();
+  
+		auto const& lhs = binary_operator->getOperand(0);
+		auto const& rhs = binary_operator->getOperand(1);
+		auto const& type = lhs->getType();
 
-                Instruction *replacement = BinaryOperator::CreateAdd(
-                    B.CreateMul(
-                        B.CreateMul(
-                            ConstantInt::get(i8, 39),
-                            B.CreateAdd(
-                                B.CreateAnd(lhs, rhs),
-                                B.CreateAdd(
-                                    B.CreateMul(
-                                        ConstantInt::get(i8, 2),
-                                        B.CreateXor(lhs, rhs)
-                                    ),
-                                    ConstantInt::get(i8, 23)
-                                )
-                            )
-                        ),
-                        ConstantInt::get(i8, 151)
-                    ),
-                    ConstantInt::get(i8, 111)
-                );
+		IRBuilder<> builder(binary_operator);
 
-                return replacement;
+		auto logAnd = builder.CreateAnd(lhs, rhs);
+		auto Xor =  builder.CreateXor(lhs, rhs);
+		auto mul2 = builder.CreateMul(ConstantInt::get(type, 2), Xor);
+		auto add = builder.CreateAdd(logAnd, mul2);
+		auto mul39 = builder.CreateMul(ConstantInt::get(type, 39), add);
+		auto add23 = builder.CreateAdd(mul39, ConstantInt::get(type, 23));
+		auto mul151 = builder.CreateMul(add23, ConstantInt::get(type, 151));
+		Instruction *replacement = BinaryOperator::CreateAdd(mul151, ConstantInt::get(type, 111));
+
+    return replacement;
 }
 
 }
